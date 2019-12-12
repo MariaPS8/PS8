0c80cbeaa90760d5dc34c9fe0be0b0c87298aaee
MariaPS8 <58433415+MariaPS8@users.noreply.github.com>
2019-12-12 23:39:37 +0300

    Add files via upload

---
+++ noname.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+ int main(){
+ 	int a,b,c,e,n,i,N; 
+     cin >> N;
+	if (N>0 && N%2==0){
+
+	  
+
+	for (int i=0;i<N;i++){
+	cout << "Mary";
+	}	
+}
+	 
+	 return 0;
+ 	
+ }
