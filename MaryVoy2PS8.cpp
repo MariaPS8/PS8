8197f54b722ed4b703422bf7600550ce7eb4748b
MariaPS8 <58433415+MariaPS8@users.noreply.github.com>
2019-12-16 01:27:38 +0300

    Add files via upload

---
+++ MaryVoy2PS8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+int p,i,N;
+cin>>N;
+vector <int>nums(N);
+for (i=0;i<N;++i){
+	cin >> nums [i];
+}
+	cin>>p;
+	cout<<p;
+		
+		if (p%9){
+			cout<<"find it";
+		}
+	
+return 0;
+}
+
